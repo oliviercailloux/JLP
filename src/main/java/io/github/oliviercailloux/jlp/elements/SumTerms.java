8fb6b9b6a6a403abb4ad8d7140c84a7b302c18ac
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2017-12-30 12:29:04 +0100

    Cleaned and improved Sum

--- src/main/java/io/github/oliviercailloux/jlp/elements/SumTerms.java
+++ src/main/java/io/github/oliviercailloux/jlp/elements/SumTerms.java
@@ -3,23 +3,25 @@ package io.github.oliviercailloux.jlp.elements;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ForwardingList;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableList.Builder;
-import com.google.common.collect.Lists;
 
 /**
  * <p>
  * A linear expression consisting of a possibly empty sum of terms, where a term
  * is a coefficient multiplying a variable. A linear object
  * {@link #equals(Object)} an other one iff they contain the same terms in the
- * same order. Order of addition is retained and reused when queried.
+ * same order.
  * </p>
+ * <p>
+ * This object allows for zero coefficients. It might be useful to store the
+ * structure a constraint of the kind ax+by, even though a might (on a given
+ * instance) equal zero.
  * <p>
  * Immutable.
  * </p>
@@ -33,13 +35,64 @@ public class SumTerms extends ForwardingList<Term> {
 	}
 
 	/**
-	 * Retrieves a linear, immutable object containing the given terms.
+	 * Returns a sum containing a single term.
+	 *
+	 * @param c1
+	 *            a finite number.
+	 * @param v1
+	 *            not <code>null</code>.
+	 * @return not <code>null</code>.
+	 */
+	static public SumTerms of(double c1, Variable v1) {
+		return new SumTerms(ImmutableList.of(Term.of(c1, v1)));
+	}
+
+	/**
+	 * Returns a sum containing two terms.
+	 *
+	 * @param c1
+	 *            a finite number.
+	 * @param v1
+	 *            not <code>null</code>.
+	 * @param c2
+	 *            a finite number.
+	 * @param v2
+	 *            not <code>null</code>.
+	 * @return not <code>null</code>.
+	 */
+	static public SumTerms of(double c1, Variable v1, double c2, Variable v2) {
+		return new SumTerms(ImmutableList.of(Term.of(c1, v1), Term.of(c2, v2)));
+	}
+
+	/**
+	 * Returns a sum containing three terms.
+	 *
+	 * @param c1
+	 *            a finite number.
+	 * @param v1
+	 *            not <code>null</code>.
+	 * @param c2
+	 *            a finite number.
+	 * @param v2
+	 *            not <code>null</code>.
+	 * @param c3
+	 *            a finite number.
+	 * @param v3
+	 *            not <code>null</code>.
+	 * @return not <code>null</code>.
+	 */
+	static public SumTerms of(double c1, Variable v1, double c2, Variable v2, double c3, Variable v3) {
+		return new SumTerms(ImmutableList.of(Term.of(c1, v1), Term.of(c2, v2), Term.of(c3, v3)));
+	}
+
+	/**
+	 * Returns a sum containing the given terms.
 	 *
 	 * @param terms
 	 *            not <code>null</code>.
-	 * @return a linear object.
+	 * @return not <code>null</code>.
 	 */
-	static public SumTerms of(Collection<Term> terms) {
+	static public SumTerms of(Iterable<Term> terms) {
 		requireNonNull(terms);
 		if (terms instanceof SumTerms) {
 			return (SumTerms) terms;
@@ -47,27 +100,13 @@ public class SumTerms extends ForwardingList<Term> {
 		return new SumTerms(terms);
 	}
 
-	static public SumTerms of(double c1, Variable v1) {
-		final List<Term> asList = Lists.newLinkedList();
-		asList.add(Term.of(c1, v1));
-		return new SumTerms(asList);
-	}
-
-	static public SumTerms of(double c1, Variable v1, double c2, Variable v2) {
-		final List<Term> asList = Lists.newLinkedList();
-		asList.add(Term.of(c1, v1));
-		asList.add(Term.of(c2, v2));
-		return new SumTerms(asList);
-	}
-
-	static public SumTerms of(double c1, Variable v1, double c2, Variable v2, double c3, Variable v3) {
-		final List<Term> asList = Lists.newLinkedList();
-		asList.add(Term.of(c1, v1));
-		asList.add(Term.of(c2, v2));
-		asList.add(Term.of(c3, v3));
-		return new SumTerms(asList);
-	}
-
+	/**
+	 * Returns a sum containing the given terms.
+	 *
+	 * @param terms
+	 *            the terms.
+	 * @return not <code>null</code>.
+	 */
 	static public SumTerms of(Term... terms) {
 		return new SumTerms(Arrays.asList(terms));
 	}
@@ -76,9 +115,21 @@ public class SumTerms extends ForwardingList<Term> {
 
 	private ImmutableList<Variable> variables;
 
-	private SumTerms(Collection<Term> terms) {
+	private SumTerms(Iterable<Term> terms) {
 		delegate = ImmutableList.copyOf(terms);
 		initVariables();
+	}
+
+	/**
+	 * Returns the terms composing this object. The only reason to use this method
+	 * is to get a reference to a list of terms that is explicitely typed as
+	 * Immutable. The returned list contains the same information as that present in
+	 * this object.
+	 *
+	 * @return not <code>null</code>.
+	 */
+	public ImmutableList<Term> asImmutableList() {
+		return delegate;
 	}
 
 	/**
@@ -93,11 +144,10 @@ public class SumTerms extends ForwardingList<Term> {
 	}
 
 	/**
-	 * Returns a string representation of the given linear expression.
+	 * Returns a short string representation of this sum. This should be used for
+	 * debug purposes only as this method gives no control on the number of decimal
+	 * digits shown.
 	 *
-	 * @param linear
-	 *            not <code>null</code>
-	 * @return not <code>null</code>, may be empty
 	 */
 	@Override
 	public String toString() {
@@ -105,11 +155,8 @@ public class SumTerms extends ForwardingList<Term> {
 	}
 
 	private void initVariables() {
-		final Builder<Variable> builder = ImmutableList.builder();
-		for (Term term : this) {
-			builder.add(term.getVariable());
-		}
-		variables = builder.build();
+		variables = this.stream().map(Term::getVariable)
+				.collect(Collectors.collectingAndThen(Collectors.toList(), ImmutableList::copyOf));
 	}
 
 	@Override
